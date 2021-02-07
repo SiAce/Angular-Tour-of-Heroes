84345dc92049ecbb5531e24820b8153c5c647afd
SiAce <siacespark@gmail.com>
2021-02-06 22:04:51 -0500

    add in-app navigation (router)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,24 +10,12 @@ import { MessageService } from "../message.service";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
   heroes: Hero[];
-
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
